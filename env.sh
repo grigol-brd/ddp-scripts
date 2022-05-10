d17f5efdf2011f084a1e5d828579db0ac1a51f7c
grigol-brd <gtchkoid@broadinstitute.org>
2022-05-10 14:28:52 +0400

    update

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 
-# set these absolute path directories according to locations on your machine
-#
+# set these absolute path directories according to locations on your machine <=====
+
 export STUDY_SERVER_DIR="D:\developer\broad\ddp-study-server"
 export ANGULAR_DIR="D:\developer\broad\ddp-angular"
 export SCRIPTS_DIR="D:\developer\broad\scripts-ddp"
-#
+
 # ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
 # | | | | | | | | | | | | | | | | | | | | | | | |
 # | | | | | | | | | | | | | | | | | | | | | | | |
@@ -30,8 +30,21 @@ export PEPPER_APIS_DIR="${STUDY_SERVER_DIR}/pepper-apis"
 export STUDY_BUILDER_CLI_DIR="${PEPPER_APIS_DIR}/studybuilder-cli"
 
 
-export CLIENT_ID="D1TNXhZChtMSUTGVFs1C2ow2kFGfMJrL"
-export CLIENT_SECRET="75YX_LMk5SAvJSXYJd6z7HWFOE8s9FH3BVIfgvKNViPI2YOwDHdTCHAbhGbzo-D3"
+function read_vault_key {
+  vault kv get -field=$1 secret/pepper/dev/v1/local/conf
+}
+
+
+# We usually have auth0 LocalDev app credentials stored in vault for local configurations
+export CLIENT_ID=$(read_vault_key auth0ClientId)
+export CLIENT_SECRET=$(read_vault_key auth0ClientSecret)
+
+
+if [[ -z $CLIENT_ID || -z $CLIENT_SECRET ]]; then
+  echo "Error: failed to read auth0 credentials from vault. Please make sure you have access to vault."
+  exit 1
+fi
+
 
 # trailing slash is a MUST! it won't be appended automatically by the pepper
 export AUTH0_DOMAIN="https://ddp-dev.auth0.com/"
